ed42b8f96be6832063b96e7eb732c362af0f1543
Sara <saraashraf55530@gmail.com>
2025-07-12 17:24:14 +0300

    Implement most recently suras

---
+++ lib/tabs/quran/most_recently_section.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:islami/tabs/quran/most_recently_item.dart';
+import 'package:islami/tabs/quran/quran_service.dart';
+import 'package:islami/tabs/quran/sura.dart';
+
+class MostRecentlySection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+    return Visibility(
+      visible: QuranService.mostRecentlySuras.isNotEmpty,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Most Recently',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            SizedBox(width: 10),
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.16,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (_, index) => MostRecentlyItem(
+                  QuranService.mostRecentlySuras.reversed.toList()[index],
+                ),
+                separatorBuilder: (_, __) => SizedBox(width: 10),
+                itemCount: QuranService.mostRecentlySuras.length,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
