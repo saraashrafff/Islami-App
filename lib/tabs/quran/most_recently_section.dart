afe6a4a6f6e548fa8ebc98a31d85d94a721c5106
Sara <saraashraf55530@gmail.com>
2025-07-13 15:04:22 +0300

    fix most recently section

--- lib/tabs/quran/most_recently_section.dart
+++ lib/tabs/quran/most_recently_section.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:islami/tabs/quran/most_recently_item.dart';
 import 'package:islami/tabs/quran/quran_service.dart';
-import 'package:islami/tabs/quran/sura.dart';
 
 class MostRecentlySection extends StatelessWidget {
   @override
